331991ac463e79687efdc2607dd233b12cd60bfb
penguibear <penguibeatz@gmail.com>
2013-10-16 20:38:21 -0500

    Create monster.java

---
+++ monster.java
@@ -0,0 +1,11 @@
+public class monster{
+	int ATK, DEF, HP;
+	String NAME;
+	public monster(String inName, int inATK, int inDEF, int inHP){
+		this.NAME = inName;
+		this.ATK = inATK;
+		this.DEF = inDEF;
+		this.HP = inHP;
+	}
+							//This is my class for creating monsters. I should use this for most of the monsters
+}
