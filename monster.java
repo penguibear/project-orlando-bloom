d7d3dd5f9d90be062676627a57389c0b90818f67
penguibear <penguibeatz@gmail.com>
2013-10-16 21:03:17 -0500

    Update monster.java
    changed the monster variables to not have "this." and added some int methods

--- monster.java
+++ monster.java
@@ -1,11 +1,18 @@
 public class monster{
 	int ATK, DEF, HP;
 	String NAME;
+
 	public monster(String inName, int inATK, int inDEF, int inHP){
-		this.NAME = inName;
-		this.ATK = inATK;
-		this.DEF = inDEF;
-		this.HP = inHP;
-	}
-							//This is my class for creating monsters. I should use this for most of the monsters
+		NAME = inName;
+		ATK = inATK;
+		DEF = inDEF;
+		HP = inHP;
+
+	}						//This is my class for creating monsters. I should use this for most of the monsters
+
+	public int getATK(){return this.ATK;}
+	public int getDEF(){return this.DEF;}
+	public int getHP(){return this.HP;}
+
+
 }
